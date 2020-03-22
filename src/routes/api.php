efd46ac7b64df425df152f81febe5d624f50cc39
ororo <farewellbbbb@gmail.com>
2020-03-22 21:56:33 +0900

    ユーザー登録機能実装

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,6 +18,7 @@ Route::group([
     'prefix' => 'auth'
 
 ], function ($router) {
+    Route::post('create', 'AuthController@create');
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
