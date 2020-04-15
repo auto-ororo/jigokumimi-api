ff572a07b43521768329c1db56e4fb4a8dfa3f0a
ororo <farewellbbbb@gmail.com>
2020-04-15 19:10:51 +0900

    masterブランチでマージされた際にデプロイされるように変更

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Jigokumimiiiiiii
+                Jigokumimi
             </div>
         </div>
     </div>
